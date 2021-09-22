a54d2d8cda603c079e626ed4a8245b0390f11dd4
udaykumar-1999 <74130632+udaykumar-1999@users.noreply.github.com>
2021-09-22 18:16:07 +0530

    Create p1.py

---
+++ pyunitest/p1.py
@@ -0,0 +1,19 @@
+class Person:
+    name = []
+    first_name = []
+
+    def set_name(self, user_name):
+        self.name.append(user_name)
+        return len(self.name) - 1
+
+    def get_name(self, user_id):
+        if user_id >= len(self.name):
+            return 'there is no such user'
+        else:
+            return self.name[user_id]
+
+
+if __name__ == '__main__':
+    person = Person()
+    print("User uday is been added with id", person.set_name('uday'))
+    print("User is associated with id 0 is", person.get_name(0))
